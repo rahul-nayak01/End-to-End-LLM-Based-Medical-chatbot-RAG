d444ce940543512cd764389588605cf1e2ff1e75
Rahul R Nayak <nayakrahul0852@gmail.com>
2026-02-01 09:24:20 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rahul R Nayak",
+    author_email="nayakrahul0852@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
